ca0dad1fcc4001bec7b054564d1ee71107e6700b
Ryan Brewster <brewster1134+tower@gmail.com>
2015-03-12 10:43:11 -0700

    engine yard

--- engine_yard/engine_yard_task.rb
+++ engine_yard/engine_yard_task.rb
@@ -1,4 +1,21 @@
 class New::EngineYardTask < New::Task
-  @@description = 'Deploy to Engine Yard'
-  @@options = {}
+  @@description = 'Deploy your Engine Yard application'
+  @@options = {
+    :account => {
+      :description => 'Name of the account in which the application and environment can be found',
+      :required => true
+    },
+    :app => {
+      :description => 'The application to deploy',
+      :required => true
+    },
+    :environment => {
+      :description => 'The environment to which you want this application to deploy',
+      :required => true
+    }
+  }
+
+  def run options
+    system "ey deploy --account='#{options[:task_options][:account]}' --app='#{options[:task_options][:app]}' --environment='#{options[:task_options][:environment]}'"
+  end
 end
